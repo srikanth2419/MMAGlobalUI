3e217dcd4173e2805ddc896f68a2be59638a81d1
chandrika31 <udhayachandrikamurugan94@gmail.com>
2022-08-08 12:10:20 +0530

    added few basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { GalleriaModule } from 'primeng/galleria';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    GalleriaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss']
+})
+export class HomepageComponent implements OnInit {
+  images: any[] = [];
+
+  responsiveOptions: any[] = [
+    {
+      breakpoint: '1024px',
+      numVisible: 5
+    },
+    {
+      breakpoint: '768px',
+      numVisible: 3
+    },
+    {
+      breakpoint: '560px',
+      numVisible: 1
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.images = [
+      {
+        previewImageSrc: '../../assets/images/shooting-script.jpg',
+        thumbnailImageSrc: '../../assets/images/shooting-script.jpg'
+      },
+      {
+        previewImageSrc: '../../assets/images/film-movie.jpg',
+        thumbnailImageSrc: '../../assets/images/film-movie.jpg'
+      },
+      {
+        previewImageSrc: '../../assets/images/film-making.jpg',
+        thumbnailImageSrc: '../../assets/images/film-making.jpg'
+      }
+    ];
+  }
+
+}
