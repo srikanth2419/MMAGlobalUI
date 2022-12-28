3a6960e0b2992d6fcc2c086691ecb5b196a421f0
Srikanth.Ramar_4810 <srikanth2419>
2022-12-28 16:07:55 +0530

    MMAGLOBAL Changes

---
+++ src/app/CONSTANTS-MODULE/pathconstants.ts
@@ -0,0 +1,7 @@
+export class Pathconstants {
+
+    public static readonly TrainingDB_Post ='TrainingDB/SaveTrainingDB' // Controller Name/Method Name
+
+    public static readonly TrainingDB_GET ='TrainingDB/GetTrainingDB' // Controller Name / Method Name 
+
+}
