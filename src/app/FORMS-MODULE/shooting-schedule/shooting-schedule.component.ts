68e041bc3d37a4a9260b6c3bbfe18d61dc169979
10572 <10572@CHNTESLTP-10572>
2023-01-20 17:26:18 +0530

    shootingschedule

--- src/app/FORMS-MODULE/shooting-schedule/shooting-schedule.component.ts
+++ src/app/FORMS-MODULE/shooting-schedule/shooting-schedule.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SelectItem } from 'primeng/api';
+import { Pathconstants } from 'src/app/CONSTANTS-MODULE/pathconstants';
 import { TableConstants } from 'src/app/CONSTANTS-MODULE/table-constants';
+import { RestapiService } from 'src/app/services/restapi.service';
 
 @Component({
   selector: 'app-shooting-schedule',
@@ -23,10 +25,51 @@ export class ShootingScheduleComponent implements OnInit {
   shootingScheduleCols: any;
   shootingScheduleDetails: any[] = [];
   selectedPerson: any[] = [];
-  constructor() { }
+  newprojectcreationData:any[] = [];
+
+
+  constructor(private restapiservice: RestapiService) { 
+  }
 
   ngOnInit(): void {
+    this.dayNightOptions = [
+      { label: 'select',value:1 },
+      { label: 'DAY', value: 2 },
+      { label: 'NIGHT', value: 3 },
+      
+    ];
+    this.designOptions = [
+      { label: 'select',value:1 },
+      { label: 'Interior', value: 2 },
+      { label: 'Exterior', value: 3 },
+      
+    ];
+    this.statusOptions = [
+      { label: 'select',value:1 },
+      { label: 'Pending', value: 2 },
+      { label: 'Started', value: 3 },
+      { label: 'Completed', value: 4 },
+      { label: 'Cancelled ', value: 5 },
+
+
+      
+    ];
     this.shootingScheduleCols = TableConstants.ShootingScheduleColumns;
+    this.restapiservice.get(Pathconstants.projectcreation_Get).subscribe(res => { this.newprojectcreationData = res })
+  }
+
+  onSelect(type: any) {
+    let projectSelection: any = [];
+
+    switch (type) {
+      case 'p':
+        this.newprojectcreationData.forEach((c: any) => {
+          projectSelection.push({ label: c.project_name, value: c.slno });
+        })
+        this.projectNameOptions = projectSelection;
+        this.projectNameOptions.unshift({ label: '-select', value: null });
+        break;
+    }
   }
 
 }
