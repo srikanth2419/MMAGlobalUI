cd9de6daca76e67f61567c336aef45d1a269cde7
Priyadharshini Jayakumar <T798@Tessolveindia.com>
2023-01-31 18:13:12 +0530

    changes in daily expenses

--- src/app/FORMS-MODULE/call-sheet/call-sheet.component.ts
+++ src/app/FORMS-MODULE/call-sheet/call-sheet.component.ts
@@ -39,14 +39,51 @@ export class CallSheetComponent implements OnInit {
   loading: boolean = false;
   RowId:any;
   responseMsg: Message[] = [];
+  selectedType: any;
+  rolemasterData:any[]=[];
   constructor(private restapiservice: RestapiService) { }
 
   ngOnInit(): void {
     this.callSheetCols = TableConstants.ShootingScheduleColumns;
     this.lodginginfocols=TableConstants.lodginginfoColumns;
     this.transportinfocols=TableConstants.transportinfoColumns;
+    this.restapiservice.get(Pathconstants.rolemaster_Get).subscribe(res => {this.rolemasterData = res})
   }
-onSave1(){
+  onSave1(){
+    const params ={
+      'slno':this.RowId,
+      'projectname':this.projectName,
+      'roleid':this.role,
+      'date':this.date,
+      'general_call_time':this.generalCallTime,
+      'shooting_call_time':this.scheduleCallTime,
+      'location_id':this.location,
+      'created_date': new Date(),
+      'flag':(this.selectedType == 1) ? true : false
+    };
+    this.restapiservice.post(Pathconstants.callinfo_Post, params).subscribe(res => {
+      if (res != null && res != undefined) {
+        this.onView1();
+        
+        this.responseMsg = [{ severity: ResponseMessage.SuccessSeverity, detail: ResponseMessage.SuccessMessage }];
+        setTimeout(() => this.responseMsg = [], 3000);
+      }
+      else {
+        this.responseMsg = [{ severity: ResponseMessage.ErrorSeverity, detail: ResponseMessage.ErrorMessage }];
+        setTimeout(() => this.responseMsg = [], 3000);
+      }
+    })
+  }
+  onView1(){
+    this.restapiservice.get(Pathconstants.callinfo_GET).subscribe(res => {
+      this.lodginginfoData = res;
+    })
+
+  }
+  onEdit1(){
+
+  }
+onSave2(){
   const params = {
     'slno': this.RowId,
     'location': this. locationName,
@@ -55,7 +92,7 @@ onSave1(){
   };
   this.restapiservice.post(Pathconstants.lodginginfo_Post, params).subscribe(res => {
     if (res != null && res != undefined) {
-      this.onView1();
+      this.onView2();
       
       this.responseMsg = [{ severity: ResponseMessage.SuccessSeverity, detail: ResponseMessage.SuccessMessage }];
       setTimeout(() => this.responseMsg = [], 3000);
@@ -66,18 +103,18 @@ onSave1(){
     }
   })
 }
-onView1(){
+onView2(){
     this.restapiservice.get(Pathconstants.lodginginfo_GET).subscribe(res => {
       this.lodginginfoData = res;
     })
 }
-onEdit1(rowData:any){
+onEdit2(rowData:any){
   this.RowId=rowData.slno;
   this.locationName=rowData.location;
   this.address=rowData.address;
   this.note=rowData.note;
 }
-onSave2(){
+onSave3(){
   const params = {
     'slno': this.RowId,
     'driver_name': this.driverName,
@@ -88,7 +125,7 @@ onSave2(){
   };
   this.restapiservice.post(Pathconstants.transportinfo_Post, params).subscribe(res => {
     if (res != null && res != undefined) {
-      this.onView1();
+      this.onView3();
       
       this.responseMsg = [{ severity: ResponseMessage.SuccessSeverity, detail: ResponseMessage.SuccessMessage }];
       setTimeout(() => this.responseMsg = [], 3000);
@@ -100,12 +137,12 @@ onSave2(){
   })
 
 }
-onView2(){
+onView3(){
   this.restapiservice.get(Pathconstants.transportinfo_GET).subscribe(res => {
     this.lodginginfoData = res;
   })
 }
-onEdit2(rowData:any){
+onEdit3(rowData:any){
   this.RowId=rowData.slno;
   this.driverName=rowData.driver_name;
   this.pickupTime=rowData.pickup_time;
