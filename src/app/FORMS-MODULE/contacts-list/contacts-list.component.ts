f1d944a983fa3a5fca11f4a6ecae36da532538cf
10572 <10572@CHNTESLTP-10572>
2023-05-11 11:54:41 +0530

    phone number validation

--- src/app/FORMS-MODULE/contacts-list/contacts-list.component.ts
+++ src/app/FORMS-MODULE/contacts-list/contacts-list.component.ts
@@ -400,5 +400,4 @@ export class ContactsListComponent implements OnInit {
              else {
             this._contactlistForm.controls['_pincode'].setErrors({ 'incorrect': true });
         }
-        }
-      }
+      }}
